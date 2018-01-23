7a622cef1827aedf7451e86a46b1d74547e59689
sabyasachi <sabyasachipalit1995@gmail.com>
2018-01-23 14:25:49 +0530

    Add Socket.io

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 io.on('connection', function () {
 	console.log('User Connected via socket');
 });
